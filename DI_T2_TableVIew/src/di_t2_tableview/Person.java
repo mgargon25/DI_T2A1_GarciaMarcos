dcc08e1374bfbc437c67f36f2507ad523bca0186
mgargon25 <marcos.garciagonzalez25@gmail.com>
2021-11-15 20:25:56 +0100

    Ejercicio TableView

---
+++ DI_T2_TableVIew/src/di_t2_tableview/Person.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_t2_tableview;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author usuario
+ */
+public class Person {
+    private final SimpleStringProperty firstName;
+        private final SimpleStringProperty lastName;
+        private final SimpleStringProperty email;
+ 
+        private Person(String fName, String lName, String email) {
+            this.firstName = new SimpleStringProperty(fName);
+            this.lastName = new SimpleStringProperty(lName);
+            this.email = new SimpleStringProperty(email);
+        }
+ 
+        public String getFirstName() {
+            return firstName.get();
+        }
+ 
+        public void setFirstName(String fName) {
+            firstName.set(fName);
+        }
+ 
+        public String getLastName() {
+            return lastName.get();
+        }
+ 
+        public void setLastName(String fName) {
+            lastName.set(fName);
+        }
+ 
+        public String getEmail() {
+            return email.get();
+        }
+ 
+        public void setEmail(String fName) {
+            email.set(fName);
+        }
+}
